e1cff6a06ccdc661284f2a705b37184e33caede4
Emilia <emiliaprzybylek@hotmail.com>
2021-09-26 20:35:14 +0200

    calculate added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const form = document.querySelector('form');
-const weight = document.querySelectorAll('input')[0];
-const height = document.querySelectorAll('input')[1];
+const weight = document.getElementById('weight');
+const height = document.getElementById('height');
 const resultPlace = document.querySelector('span');
 
 form.addEventListener('submit', (e) => {
+
     e.preventDefault();
-    resultPlace.innerText = (parseFloat(weight.value) / (parseFloat(height.value) * parseFloat(height.value) / 10000));
+    resultPlace.innerText = Math.round(parseFloat(weight.value) / (parseFloat(height.value) * parseFloat(height.value) / 10000) * 100) / 100;
     console.log(resultPlace)
 });
 

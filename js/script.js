4984c3e8b919225d2760a11ebd7a7dfc8b55bb8b
Emilia <emiliaprzybylek@hotmail.com>
2021-09-26 14:01:40 +0200

    script added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const form = document.querySelector('form');
+const weight = document.querySelectorAll('input')[0];
+const height = document.querySelectorAll('input')[1];
+const resultPlace = document.querySelector('span');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    resultPlace.innerText = (parseFloat(weight.value) / (parseFloat(height.value) * parseFloat(height.value) / 10000));
+    console.log(resultPlace)
+});
+
+
+

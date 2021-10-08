5c6b279f2aeca1210fe6f59edb45596881d77201
Emilia <emiliaprzybylek@hotmail.com>
2021-10-08 14:44:02 +0200

    script modified

--- js/script.js
+++ js/script.js
@@ -11,21 +11,111 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     resultPlace.innerText = Math.round(parseFloat(weight.value) / (parseFloat(height.value) * parseFloat(height.value) / 10000) * 100) / 100;
 
-    if (resultPlace.innerText <= 16) {
-        description.innerText = "Wygłodzenie";
-     } if (resultPlace.innerText >= 17) {
-        description.innerText = "Niedowaga";
-     } if (resultPlace.innerText >= 18.5) {
-        description.innerText = "Wartość prawidłowa";
-     } if (resultPlace.innerText >= 25) {
-        description.innerText = "Nadwaga";
-     } if (resultPlace.innerText >= 30) {
-        description.innerText = "I stopień otyłości";
-     } if (resultPlace.innerText >= 35) {
-        description.innerText = "II stopień otyłości";
-     } if (resultPlace.innerText >= 40){
-        description.innerText = "Skrajna otyłość";
+  let result = "";
+
+     if (resultPlace.innerText < 16) {
+        result = "Wygłodzenie";
+     } 
+     if (resultPlace.innerText >= 16 ) {
+        result = "Wychudzenie";
+     } 
+     if (resultPlace.innerText >= 17) {
+        result = "Niedowaga";
+     } 
+     if (resultPlace.innerText >= 18.5) {
+        result = "Wartość prawidłowa";
+     } 
+     if (resultPlace.innerText >= 25) {
+        result = "Nadwaga";
+     } 
+     if (resultPlace.innerText >= 30) {
+        result = "I stopień otyłości";
+     } 
+     if (resultPlace.innerText >= 35) {
+        result = "II stopień otyłości";
+     } 
+     if (resultPlace.innerText >= 40) {
+        result = "Skrajna otyłość";
      }
-});
+      description.innerText = result;
+     
+   });
+   
+
+   //   if (resultPlace.innerText <= 16) {
+   //      result = "Wygłodzenie";
+   //   } 
+   //   else if (resultPlace.innerText >= 17) {
+   //      result = "Niedowaga";
+   //   } 
+   //   else if (resultPlace.innerText >= 18.5) {
+   //      result = "Wartość prawidłowa";
+   //   } 
+   //   else if (resultPlace.innerText >= 25) {
+   //      result = "Nadwaga";
+   //   } 
+   //   else if (resultPlace.innerText >= 30) {
+   //      result = "I stopień otyłości";
+   //   } 
+   //   else if (resultPlace.innerText >= 35) {
+   //      result = "II stopień otyłości";
+   //   } 
+   //   else (resultPlace.innerText >= 40) {
+   //      result = "Skrajna otyłość";
+   //   }
+
+   //  description.innerText = result;
 
 
+// ///////////////////////////////////////////////
+//     if (resultPlace.innerText >= 40) {
+//       result = "Skrajna otyłość";
+//    } 
+//    else if (resultPlace.innerText >= 35) {
+//       result = "II stopień otyłości";
+//    } 
+//    else if (resultPlace.innerText >= 30) {
+//       result = "I stopień otyłości";
+//    } 
+//    else if (resultPlace.innerText >= 25) {
+//       result = "Nadwaga";
+//    } 
+//    else if (resultPlace.innerText >= 18.5) {
+//       result = "Wartość prawidłowa";
+//    } 
+//    else if (resultPlace.innerText >= 17) {
+//       result = "Niedowaga";
+//    } 
+//    else (resultPlace.innerText <= 16) {
+//       result = "Wygłodzenie";
+//    }
+
+//   description.innerText = result;
+
+// /////////////////////////////////////////////
+
+// if (resultPlace.innerText < 16) {
+//    result = "Wygłodzenie";
+// } 
+// else if (resultPlace.innerText >= 16 && bmi < 16.99) {
+//    result = "Wychudzenie";
+// } 
+// else if (resultPlace.innerText >= 17 && bmi < 18.49) {
+//    result = "Niedowaga";
+// } 
+// else if (resultPlace.innerText >= 18.5 && bmi < 24.99) {
+//    result = "Wartość prawidłowa";
+// } 
+// else if (resultPlace.innerText >= 25 && bmi < 29.99) {
+//    result = "Nadwaga";
+// } 
+// else if (resultPlace.innerText >= 30 && bmi < 34.99) {
+//    result = "I stopień otyłości";
+// } 
+// else if (resultPlace.innerText >= 35 && bmi < 39.99) {
+//    result = "II stopień otyłości";
+// } 
+// else (resultPlace.innerText >= 40) {
+//    result = "Skrajna otyłość";
+// }
+//  description.innerText = result;
